ba130e465cb92c00ae173c92a064bc4029c88532
UPSCrepo <110414070+UPSCrepo@users.noreply.github.com>
2024-04-09 18:53:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const express = require('express')
+const request = require("request");
+const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000
+const url = "https://google.com";
+
+request(url, cb);
+function cb(err, response, html) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(html);
+  }
+}
 
 app.get('/', (req, res) => {
   res.json({
